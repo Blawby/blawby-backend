864719e5be899a38066450121f5f9bd7af2aecd7
giteshk11 <writemail2gitesh@gmail.com>
2026-01-21 10:26:16 +0530

    feat: Add bearer plugin to the better-auth configuration.

--- src/shared/auth/better-auth.ts
+++ src/shared/auth/better-auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { anonymous, organization } from 'better-auth/plugins';
+import { anonymous, bearer, organization } from 'better-auth/plugins';
 import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as schema from '@/schema';
 import { AUTH_CONFIG } from '@/shared/auth/config/authConfig';
@@ -25,6 +25,7 @@ const betterAuthInstance = (
       usePlural: true,
     }),
     plugins: [
+      bearer(),
       organization({
         ac: organizationAccessController,
         roles: organizationRoles,
