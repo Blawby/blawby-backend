e8aaa603abc8bde5f1c32038c5ad7407d6067db7
Paul Chris Luke <paulchrisluke@users.noreply.github.com>
2026-01-28 13:13:49 +0800

    Return organization_id in upsert practice details

--- src/modules/practice/types/practice-details.types.ts
+++ src/modules/practice/types/practice-details.types.ts
@@ -5,7 +5,7 @@ export type PracticeDetailsResponse = Omit<
   PracticeDetails,
   'id' | 'organization_id' | 'user_id' | 'address_id' | 'created_at' | 'updated_at' | 'services'
 > & {
-  organizationId: string;
+  organization_id: string;
   address?: AddressData | null;
   services: Array<{ id: string; name: string; key: string }>;
   name?: string;
