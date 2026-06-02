38f91f2855af91f817f3404afa1d07c707843436
giteshk11 <writemail2gitesh@gmail.com>
2026-06-02 09:24:24 +0530

    feat: refactor last_stripe_event_created_at column addition for payouts table with update and default setting

--- src/shared/database/migrations/0061_familiar_apocalypse.sql
+++ src/shared/database/migrations/0061_familiar_apocalypse.sql
@@ -1,1 +1,4 @@
-ALTER TABLE "payouts" ADD COLUMN "last_stripe_event_created_at" timestamp with time zone DEFAULT now() NOT NULL;
+ALTER TABLE "payouts" ADD COLUMN "last_stripe_event_created_at" timestamp with time zone;--> statement-breakpoint
+UPDATE "payouts" SET "last_stripe_event_created_at" = "stripe_created_at" WHERE "last_stripe_event_created_at" IS NULL;--> statement-breakpoint
+ALTER TABLE "payouts" ALTER COLUMN "last_stripe_event_created_at" SET DEFAULT now();--> statement-breakpoint
+ALTER TABLE "payouts" ALTER COLUMN "last_stripe_event_created_at" SET NOT NULL;
